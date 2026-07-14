0c61d87985728d17a40569cc36a282373a1ac7c3
ConfidentAndrew <cagliariadriel@gmail.com>
2026-07-14 18:10:11 -0300

    Adiciona primeiro menu do sistema

---
+++ menu.py
@@ -0,0 +1,9 @@
+def mostrar_menu():
+    print("====================")
+    print("       LifeOS")
+    print("====================")
+    print()
+    print("1 - Tarefas")
+    print("2 - Hábitos")
+    print("3 - Metas")
+    print("4 - Sair")
