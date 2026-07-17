a1db00fe8410c6c8683e45701740e1160a9a473d
ConfidentAndrew <cagliariadriel@gmail.com>
2026-07-16 22:15:06 -0300

    Implementa gerenciamento de tarefas

--- modulos/tarefas.py
+++ modulos/tarefas.py
@@ -5,6 +5,7 @@ def abrir():
         print("===== MÓDULO DE TAREFAS =====")
         print("1 - Adicionar tarefa")
         print("2 - Listar tarefa")
+        print("3 - Remover tarefa")
         print("0 - Voltar")
 
         opcao = input("\nEscolha uma opcao: ")
@@ -19,6 +20,17 @@ def abrir():
            
            for numero, tarefa in enumerate(tarefas):
               print(f"{numero + 1}. {tarefa}")
+
+        elif opcao == "3":
+             try:
+                numero = int(input("\nDigite o número da tarefa que deseja remover: "))
+             
+                removida = tarefas.pop(numero - 1)
+
+                print(f"\nTarefa removida: {removida}")
+
+             except:
+                print("\nNão foi possível remover essa tarefa.")
 
         elif opcao == "0":
           break 
