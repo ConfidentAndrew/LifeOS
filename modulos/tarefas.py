e19261bdf1379dd4bbfec2a463477539e77f2cfc
ConfidentAndrew <cagliariadriel@gmail.com>
2026-07-20 18:44:22 -0300

    feat: adiciona conclusão de tarefas

--- modulos/tarefas.py
+++ modulos/tarefas.py
@@ -1,6 +1,8 @@
 from modulos import arquivos
 
+
 tarefas = arquivos.carregar_tarefas()
+
 
 def abrir():
     while True:    
@@ -8,20 +10,25 @@ def abrir():
         print("1 - Adicionar tarefa")
         print("2 - Listar tarefa")
         print("3 - Remover tarefa")
+        print("4 - Concluir tarefa")
         print("0 - Voltar")
 
         opcao = input("\nEscolha uma opcao: ")
 
         if opcao == "1":
            nome = input("\nDigite a tarefa: ")
-           tarefas.append(nome)
+           tarefa = {
+              "nome": nome,
+              "concluida": False
+           }
+           tarefas.append(tarefa)
            arquivos.salvar_tarefas(tarefas)
            print("\nTarefa adicionada com sucesso!")
 
         elif opcao == "2":
            print("\n ===== SUAS TAREFAS =====")           
            for numero, tarefa in enumerate(tarefas):
-              print(f"{numero + 1}. {tarefa}")
+              print(f"{numero + 1}. {tarefa['nome']}")
 
         elif opcao == "3":
              try:
@@ -32,6 +39,13 @@ def abrir():
              except:
                 print("\nNão foi possível remover essa tarefa.")
 
+        elif opcao == "4":
+           numero = int(input("\nDigite o número da tarefa concluída: "))
+           tarefa = tarefas[numero - 1]
+           tarefa["concluida"] = True
+           arquivos.salvar_tarefas(tarefas)
+           print("\nTarefa concluída com sucesso!")
+           
         elif opcao == "0":
           break 
         
