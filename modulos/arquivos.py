01ad5d0a2bd01510be8090b79f242d71244f0f22
ConfidentAndrew <cagliariadriel@gmail.com>
2026-07-23 00:12:15 -0300

    feat: adiciona módulo completo de gerenciamento de hábitos

--- modulos/arquivos.py
+++ modulos/arquivos.py
@@ -7,3 +7,11 @@ def carregar_tarefas():
 def salvar_tarefas(tarefas):
     with open("dados/tarefas.json", "w", encoding="utf-8") as arquivo:
         json.dump(tarefas, arquivo, ensure_ascii=False, indent=4)
+
+def carregar_habitos():
+    with open("dados/habitos.json", "r", encoding="utf-8") as arquivo:
+        return json.load(arquivo)
+
+def salvar_habitos(habitos):
+    with open("dados/habitos.json", "w", encoding="utf-8") as arquivo:
+        json.dump(habitos,arquivo, ensure_ascii=False, indent=4)
--- modulos/habitos.py
+++ modulos/habitos.py
@@ -1,2 +1,52 @@
+from modulos import arquivos
+
+habitos = arquivos.carregar_habitos()
+
+
 def abrir():
-    print("\n===== MÓDULO DE HÁBITOS =====")
+    while True:
+        print("\n===== MÓDULO DE HÁBITOS =====")
+        print("1 - Adicionar hábito")
+        print("2 - Listar hábitos")
+        print("3 - Remover hábito")
+        print("4 - Marcar hábito como realizado hoje")
+        print("0 - Voltar")
+
+        opcao = input("\nEscolha uma opção: ")
+
+        if opcao == "1":
+            nome = input("\nDigite o hábito: ")
+            novo_habito = {
+                "nome": nome,
+                "concluido_hoje": False
+            }
+            habitos.append(novo_habito)
+            arquivos.salvar_habitos(habitos)
+            print("\nHábito adicionado com sucesso!")
+
+        elif opcao == "2":
+            print("\n===== SEUS HÁBITOS =====")
+            for numero, habito in enumerate(habitos):
+                if habito["concluido_hoje"]:
+                    status = "✅"
+                else:
+                    status = "⌛"
+                print(f"{numero + 1}. {status} {habito['nome']}")
+
+        elif opcao == "3":
+            numero = int(input("\nDigite o número do hábito: "))
+            removido = habitos.pop(numero - 1)
+            arquivos.salvar_habitos(habitos)
+            print(f"\nHábito removido com sucesso: {removido['nome']}")
+
+        elif opcao == "4":
+            numero = int(input("\nDigite o número do hábito: "))
+            habitos[numero - 1]["concluido_hoje"] = True
+            arquivos.salvar_habitos(habitos)
+            print("\nHábito marcado como realizado hoje")
+
+        elif opcao == "0":
+            break
+
+        else:
+            print("\nOpção inválida") 
